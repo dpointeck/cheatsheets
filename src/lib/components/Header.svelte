38721bc7e8bc6e76c584b76367f849731df3bc4f
Daniel Pointecker <daniel.pointecker@gmail.com>
2023-04-21 12:07:51 +0200

    Add styling

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,22 @@
+<header>
+	<nav>
+		<a href="/">Home</a>
+		<a href="/vim">Vim</a>
+	</nav>
+</header>
+
+<style>
+	header {
+		background-color: #1d4ed8;
+		color: #fff;
+		padding: 1rem;
+	}
+
+	@media (prefers-color-scheme: dark) {
+		header {
+			background-color: #eab308;
+			color: #083344;
+			padding: 1rem;
+		}
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
-<nav>
-    <a href="/">Home</a>
-    <a href="/vim">Vim</a>
-</nav>
+<script>
+    import "../variables.css";	
+    import "../reset.css";
+    import "../app.css";
+
+    import Header from "$lib/components/Header.svelte";
+</script>
+<Header />
 <slot />
